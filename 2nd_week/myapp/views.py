ee26c6933097031c8a69f3565c1ae74bbd872263
Talandar <talandar66@protonmail.com>
2023-03-30 17:53:21 +0200

    views

--- 2nd_week/myapp/views.py
+++ 2nd_week/myapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 
@@ -7,3 +6,10 @@ def home(request):
     return HttpResponse("<h1> Hello From App </h1>")
 
 
+def say_hello(request):
+    return HttpResponse("<h1> Say hello From App </h1>")
+
+
+def menu(request):
+    text = """ <h1> this is menu </h1> """
+    return HttpResponse(text)
